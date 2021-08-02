15f52d0102387841561969cc73465773aa3b699f
Trusov Nikolay <kazyna666@gmail.com>
2021-08-02 21:00:01 +0300

    task #2.1

--- Students and mentors.py
+++ Students and mentors.py
@@ -6,11 +6,10 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-        self.courses_attached = []
-        self.class_being_mentor = []
+        self.courses_attache = []       
 
     def lect_grade(self, lecturer, course, course_grade):
-        if isinstance(lecturer, Lecturer) and course in self.courses_attached and course in lecturer.class_being_mentor:
+        if isinstance(lecturer, Lecturer) and course in self.courses_attache and course in lecturer.class_being_mentor:
             if course in lecturer.grades_for_lecturer:
                 lecturer.grades_for_lecturer[course] +=[course_grade]
             else:
@@ -23,14 +22,14 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
         
-    # def rate_hw(self, student, course, grade):
-    #     if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-    #         if course in student.grades:
-    #             student.grades[course] += [grade]
-    #         else:
-    #             student.grades[course] = [grade]
-    #     else:
-    #         return 'Ошибка'
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 class Lecturer(Mentor):
     def __init__(self, name, surname):
@@ -38,6 +37,7 @@ class Lecturer(Mentor):
         self.surname = surname
         self.courses_attached = []
         self.grades_for_lecturer = {}
+        self.class_being_mentor = []
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -63,5 +63,16 @@ cool_mentor.courses_attached += ['Python']
 cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
+
+best_lector = Lecturer('Red', 'Man')
+best_lector.class_being_mentor += ['Python']
+
+cool_student = Student('lifa', 'Ema', 'your_gender')
+cool_student.courses_attache += ['Python']
+
+cool_student.lect_grade(best_lector, 'Python', 9)
+cool_student.lect_grade(best_lector, 'Python', 8)
+cool_student.lect_grade(best_lector, 'Python', 7)
  
 print(best_student.grades)
+print(best_lector.grades_for_lecturer)
